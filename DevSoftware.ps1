c35234ee1bb2199fa77957b5b33155c4f1815a6e
Tam Mai <tammai@buffalo.edu>
2018-05-24 22:49:34 -0400

    create repo

---
+++ DevSoftware.ps1
@@ -0,0 +1,13 @@
+﻿#Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco feature enable -n allowGlobalConfirmation
+
+#Visual Studio Code
+choco install visualstudiocode
+
+#NodeJS
+choco install nodejs.install
+
+
+
